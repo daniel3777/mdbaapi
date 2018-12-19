3feded109a09a5e6bc26151c8888e45bcde0e55c
danny <danielbamal622@gmail.com>
2018-12-19 11:43:09 +0530

    spring Boot project added

---
+++ src/main/java/com/danfa/mdbaapi/MdbaapiApplication.java
@@ -0,0 +1,14 @@
+package com.danfa.mdbaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdbaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdbaapiApplication.class, args);
+	}
+
+}
+
